50c5ebf13082fad203c0bf8250cce07e866b782d
thallysbraz <thallysbraz3@gmail.com>
2020-01-08 12:12:52 -0300

    adicionando styles

--- cardfy/src/App.js
+++ cardfy/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 import GlobalStyled from "./styles/global";
+import Board from "./components/Board";
+import Card from "./components/Card";
 import Header from "./components/Header";
+import List from "./components/List";
 
 function App() {
   return (
     <>
       <Header />
+      <Board />
+
       <GlobalStyled />
     </>
   );
