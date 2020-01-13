68af0d0fb69ce50903a563ca368dfa2406f7949d
thallysbraz <thallysbraz3@gmail.com>
2020-01-13 10:05:35 -0300

    adicionando efeito de arrastar card

--- cardfy/src/App.js
+++ cardfy/src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
+import { DndProvider } from "react-dnd";
+import HTML5BACKEND from "react-dnd-html5-backend";
 
 import GlobalStyled from "./styles/global";
 import Board from "./components/Board";
-import Card from "./components/Card";
 import Header from "./components/Header";
-import List from "./components/List";
+//import Card from "./components/Card";
+//import List from "./components/List";
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5BACKEND}>
       <Header />
       <Board />
 
       <GlobalStyled />
-    </>
+    </DndProvider>
   );
 }
 
