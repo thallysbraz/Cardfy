24af8ae1b5e52a4b313e3667051f9b4e4b66d6ca
thallysbraz <thallysbraz3@gmail.com>
2020-01-13 09:43:54 -0300

    adicionando conteudo

--- cardfy/src/components/List/index.js
+++ cardfy/src/components/List/index.js
@@ -3,14 +3,16 @@ import { MdAdd } from "react-icons/md";
 import Card from "../Card";
 import { Container } from "./styles";
 
-export default function List() {
+export default function List({ data }) {
   return (
     <Container>
       <header>
-        <h2>Tarefas</h2>
-        <button type="button">
-          <MdAdd size={24} color="#FFF" />
-        </button>
+        <h2>{data.title}</h2>
+        {data.creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#FFF" />
+          </button>
+        )}
       </header>
 
       <ul>
