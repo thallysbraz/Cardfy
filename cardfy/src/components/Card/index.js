7730a0ea3acdd0f2d52387322c5b6f28dd6d1747
thallysbraz <thallysbraz3@gmail.com>
2020-01-13 11:47:27 -0300

    troca de lista

--- cardfy/src/components/Card/index.js
+++ cardfy/src/components/Card/index.js
@@ -19,12 +19,15 @@ export default function Card({ data, index, listIndex }) {
     accept: "CARD",
     hover(item, monitor) {
       const draggedListIndex = item.listIndex;
-      //const targetListIndex = listIndex;
+      const targetListIndex = listIndex;
 
       const draggedIndex = item.index;
       const targetIndex = index;
 
-      if (draggedIndex === targetIndex) {
+      if (
+        draggedIndex === targetIndex &&
+        draggedListIndex === targetListIndex
+      ) {
         return;
       }
       //Calculos de posição
@@ -40,9 +43,10 @@ export default function Card({ data, index, listIndex }) {
       if (draggedIndex > targetIndex && draggedTop > targetCenter) {
         return;
       }
-      move(draggedListIndex, draggedIndex, targetIndex);
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
 
       item.index = targetIndex;
+      item.listIndex = targetListIndex;
     }
   });
 
